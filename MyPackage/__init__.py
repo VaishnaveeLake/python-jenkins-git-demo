a13a2018051e0ea3d16c6e1f95c323854d715cac
VaishnaveeLake <1997lakevaishu@gmail.com>
2021-02-10 12:58:20 +0530

    first commit

--- MyPackage/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 25 11:43:21 2021
-
-@author: VaishnaveeLake
-"""
-
--- code.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 27 17:12:37 2021
-
-@author: VaishnaveeLake
-"""
-
-a,b,c=1,2,3
-print(a,b,c)
